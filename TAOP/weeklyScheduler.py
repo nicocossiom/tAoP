f3a5308f9280c5ef4174c8db0734d71c756c8a78
NicoCossio <nicocossiom@gmail.com>
2021-10-13 12:07:47 +0200

    almost done with weeklyscheduler, todo: print method broken also: added Assigncookies and input examples for weekly

--- TAOP/weeklyScheduler.py
+++ TAOP/weeklyScheduler.py
@@ -9,63 +9,63 @@ class Partido:
         self.hora_fin = hora_fin
     
     def items(self): 
-        return equipo1 +" "+  equipo2 +" "+  hora_inicio +" "+  hora_fin
+        return equipo1 +" "+  equipo2 +" "+  str(int(hora_inicio)) +" "+  str(int(hora_fin))
         
 horario = dict({}) #<diapartido, lista_partidos>
 horas = dict({}) #<diapartido, lista_horarios en un dia partido>
-num_matches = 0
 
 def printPartidos (lista: list): 
     for partido in lista: 
         print(partido.items())
 
-def conflict_checker(horasp: list): 
+def conflict_checker(horasp: list, horario:dict): 
     '''
     This function recieves a list of arrays whose elements define integer ranges,
     it sorts them, finds intersections between contiguous ranges. If found deletes entry 
     from list that coincides
     '''
-    i=1
-    result = []
-    horasp.sort()
-    while(i < len(horasp)):
-        if( horasp[i-1][1] > horasp[i][0] ): 
-            horasp.pop(i+1)
-            i = i+2 #we skip element as it already coincides with other match 
-        i=i+1
-    
-
+    for item in list(zip(horasp.keys(),horasp.values())):
+        i=1
+        #sorts [[int_lower_1, int_upper_1],...] by ascending order in terms of int_lower_x, x being the index of any element of the tuple of lists 
+        listaOrdenada = sorted(item[1], key = lambda lower:lower[0])
+        # print(listaOrdenada)
+        while(i < len(listaOrdenada)):
+            if( listaOrdenada[i-1][1] > listaOrdenada[i][0] ): 
+                horario[item[0]].pop(i)
+                i = i+2 #we skip element as it already coincides with other match 
+            i=i+1
 
         
         
 
 # print("Meter datos de partidos, presione ctrl+d cuando termine: ") 
-file = open("input", "r")
+file = open("/mnt/c/Users/nicol/Documents/UPM/tAoP/TAOP/input", "r")
 data = file.readlines()
+nlines = data.count('\n')-1
+counter,num = 0,0
 # data = sys.stdin.read()   # Use Ctrl d to stop the input 
 for line in data:
+    counter = counter + 1
     spline = line.split()
     if line == "000000 0" or line == "\n": 
+        conflict_checker(horas, horario) #for last date
         for i,(date,lista) in enumerate(horario.items()): 
             print(date[0] + str(len(lista)))
             printPartidos(lista)
+        print("\n")
         sys.exit()
     elif (spline[0].isdigit()):
-        if bool(horario) is not False: 
-            conflict_checker(horario[date]) #if not first element
+        if ( bool(horario) is not False or counter+num is nlines): 
+            conflict_checker(horas, horario) #if not first element
         date = spline[0]
-        horario[date] = listaPartidos = []
-        horas[date] = hora_array = []
+        num = spline[1]
+        horario[date] = []
+        horas[date] = []
     else:     
         equipo1 = spline[0]
         equipo2 = spline[1]
         hora_inicio = spline[2]
         hora_fin = spline[3]
         partido = Partido(equipo1, equipo2, hora_fin, hora_inicio)
-        listaPartidos.append(partido)
-        hora_array.append([int(hora_inicio), int(hora_fin)])
-
-
-
-
-    #cosassss
+        horario[date].append(partido)
+        horas[date].append([int(hora_inicio), int(hora_fin)])
