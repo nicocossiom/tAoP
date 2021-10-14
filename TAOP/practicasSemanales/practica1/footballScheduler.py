c9e23ed8c205924923f2192475b7c63c8e64486a
NicoCossio <nicocossiom@gmail.com>
2021-10-14 11:11:38 +0200

    finished football schedluer

--- TAOP/practicasSemanales/practica1/footballScheduler.py
+++ TAOP/practicasSemanales/practica1/footballScheduler.py
@@ -4,17 +4,18 @@ class Partido:
     def __init__ (self, equipo1, equipo2, hora_inicio, hora_fin): 
         self.equipo1 = equipo1
         self.equipo2 = equipo2
-        self.hora_inicio = int(hora_inicio)
-        self.hora_fin = int(hora_fin)
+        self.hora_inicio = hora_inicio
+        self.hora_fin = hora_fin
     
     def toString(self): 
-        return self.equipo1 +" "+ self.equipo2 +" "+ str(int(self.hora_inicio)) +" "+  str(int(self.hora_fin)) + "\n"
+        return self.equipo1 +" "+ self.equipo2 +" "+ self.hora_inicio +" "+  self.hora_fin + "\n"
 
 def listaPrinter(listaP: list):
     res = ""
     for partido in listaP:
         res = res + partido.toString()
     return res.rstrip()
+
 def conflict_checker(horario:dict): 
     '''
     This function recieves a list of arrays whose elements define integer ranges,
@@ -26,7 +27,7 @@ def conflict_checker(horario:dict):
         #sorts [[int_lower_1, int_upper_1],...] by ascending order in terms of int_lower_x, x being the index of any element of the tuple of lists 
         listaOrdenada = sorted(listaP, key = lambda lower:lower.hora_fin)
         while(i < len(listaOrdenada)-1):
-            if( listaOrdenada[i].hora_fin > listaOrdenada[i+1].hora_inicio ): 
+            if( int(listaOrdenada[i].hora_fin) > int(listaOrdenada[i+1].hora_inicio) ): 
                 del listaOrdenada[i+1]
             else : i=i+1
         print( date + " " + str(len(listaOrdenada)) + "\n" + listaPrinter(listaOrdenada))
@@ -34,17 +35,14 @@ def conflict_checker(horario:dict):
 
         
         
-
 # file = open("/mnt/c/users/nicol/documents/upm/taop/taop/input", "r")
 # data = file.readlines()
-data = sys.stdin.readlines()
+#data = sys.stdin.readlines()
 horario = dict({}) #<diapartido, lista_partidos>
-for line in data:
+line = sys.stdin.readline()
+while line != "000000 0\n":
     spline = line.split()
-    if line == "000000 0": 
-        conflict_checker(horario)
-        sys.exit()
-    elif (spline[0].isdigit()):
+    if (spline[0].isdigit()):
         date = spline[0]
         num = spline[1]
         horario[date] = []
@@ -54,4 +52,9 @@ for line in data:
         hora_inicio = spline[2]
         hora_fin = spline[3]
         partido = Partido(equipo1, equipo2, hora_inicio, hora_fin)
-        horario[date].append(partido)        
+        horario[date].append(partido)   
+
+# all matches read, run checker
+conflict_checker(horario)
+sys.exit() 
+            
