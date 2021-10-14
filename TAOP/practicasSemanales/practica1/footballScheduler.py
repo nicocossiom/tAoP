ebee8a59a423bbe1f351c7f66dd4e29970ee8f75
Nico <nicocossiom@gmail.com>
2021-10-14 18:02:18 +0200

    optimized to run on a single date bases, using only a list and not dict

--- TAOP/practicasSemanales/practica1/footballScheduler.py
+++ TAOP/practicasSemanales/practica1/footballScheduler.py
@@ -16,45 +16,43 @@ def listaPrinter(listaP: list):
         res = res + partido.toString()
     return res.rstrip()
 
-def conflict_checker(horario:dict): 
+def conflict_checker(diaP:list, date:str): 
     '''
     This function recieves a list of arrays whose elements define integer ranges,
     it sorts them, finds intersections between contiguous ranges. If found deletes entry 
     from list that coincides
     '''
-    for date, listaP in horario.items():
-        i=0
-        #sorts [[int_lower_1, int_upper_1],...] by ascending order in terms of int_lower_x, x being the index of any element of the tuple of lists 
-        listaOrdenada = sorted(listaP, key = lambda lower:lower.hora_fin)
-        while(i < len(listaOrdenada)-1):
-            if( int(listaOrdenada[i].hora_fin) > int(listaOrdenada[i+1].hora_inicio) ): 
-                del listaOrdenada[i+1]
-            else : i=i+1
-        print( date + " " + str(len(listaOrdenada)) + "\n" + listaPrinter(listaOrdenada))
-    print("000000 0")
+    i=0
+    #sorts [[int_lower_1, int_upper_1],...] by ascending order in terms of int_lower_x, x being the index of any element of the tuple of lists 
+    #diaP.sort(key = lambda lower:lower.hora_fin)
+    listaOrdenada = sorted(diaP, key = lambda lower:lower.hora_fin)
+    while(i < len(diaP)-1):
+        if( int(diaP[i].hora_fin) > int(diaP[i+1].hora_inicio) ): 
+            del diaP[i+1]
+        else : i=i+1
+    print( date + " " + str(len(listaOrdenada)) + "\n" + listaPrinter(listaOrdenada))
+    
 
         
-        
-# file = open("/mnt/c/users/nicol/documents/upm/taop/taop/input", "r")
-# data = file.readlines()
-#data = sys.stdin.readlines()
-horario = dict({}) #<diapartido, lista_partidos>
+diaP = []
 line = sys.stdin.readline()
 while line != "000000 0\n":
     spline = line.split()
     if (spline[0].isdigit()):
+        if( bool(diaP) is not False):
+             conflict_checker(diaP, date)
         date = spline[0]
         num = spline[1]
-        horario[date] = []
+        diaP = [] #reset list
     else:     
         equipo1 = spline[0]
         equipo2 = spline[1]
         hora_inicio = spline[2]
         hora_fin = spline[3]
         partido = Partido(equipo1, equipo2, hora_inicio, hora_fin)
-        horario[date].append(partido)   
-
-# all matches read, run checker
-conflict_checker(horario)
+        diaP.append(partido)   
+    line = sys.stdin.readline()
+conflict_checker(diaP,date) #for last matchday       
+print("000000 0")
 sys.exit() 
             
