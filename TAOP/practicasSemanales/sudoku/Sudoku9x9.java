222fb8b863a3f76920b50d139ba9b409afbf751a
NicoCossio <nicocossiom@gmail.com>
2021-11-11 22:52:35 +0100

    Queda mucho, aclarar que es Board y quien implementa PartialSolution

--- TAOP/practicasSemanales/sudoku/Sudoku9x9.java
+++ TAOP/practicasSemanales/sudoku/Sudoku9x9.java
@@ -1,9 +1,8 @@
 package TAOP.practicasSemanales.sudoku;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
+import com.google.common.collect.Sets;
 
 public class Sudoku9x9 {
     public class Cell{
@@ -11,32 +10,98 @@ public class Sudoku9x9 {
         private int fila;
         private int columna;
         private Set<Integer> possibleValues;
-        public Cell(char valor, int fila, int columna){
-            this.valor = (valor != '-')? Character.getNumericValue(valor) : null;
+        private int cuadrante;
+
+        public Cell(int fila, int columna){
             this.fila = fila;
             this.columna = columna;
-            this.possibleValues = new HashSet<Integer>(); 
+            this.possibleValues = new HashSet<Integer>();
+            this.cuadrante = calcCuadrado();
         }
-        public int freedom(Board tablero ){
+        public int freedom( Board tablero ){
             int result = 0;
             boolean done = false;
-            while (!done){
-                
-            }
             return result;
         }
+        public boolean add ( Board tablero, int valAProbar){
+            //Si se consiguen añadir a las 3 es que es un valor posible 
+            return (tablero.setCuadrantes[this.cuadrante].add(valAProbar) && 
+                    tablero.setsColumnas[this.columna].add(valAProbar) && 
+                    tablero.setsFilas[this.fila].add(valAProbar));
+        }
+        public void valores (){
+            
+        }
+        
+        private int calcCuadrado(){
+            // +-----+---+---+---+---+---+---+---+---+---+
+            // | i\j | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
+            // +-----+---+---+---+---+---+---+---+---+---+
+            // |  0  |     0     |     1     |     2     |
+            // +-----+           |           |           |
+            // |  1  |           |           |           |
+            // +-----+           |           |           |
+            // |  2  |           |           |           |
+            // +-----+-----------+-----------+-----------+
+            // |  3  |     3     |     4     |     5     |
+            // +-----+           |           |           |
+            // |  4  |           |           |           |
+            // +-----+           |           |           |
+            // |  5  |           |           |           |
+            // +-----+-----------+-----------+-----------+
+            // |  6  |     6     |     7     |     8     |
+            // +-----+           |           |           |
+            // |  7  |           |           |           |
+            // +-----+           |           |           |
+            // |  8  |           |           |           |
+            // +-----+-----------+-----------+-----------+
+            int f = this.fila;
+            int c = this.columna;
+            if ( f < 3 ){
+                if (c < 3 ) return 0;
+                if ( c < 6 ) return 1;
+                else return 2; 
+            }
+            if ( f < 6 ){
+                if (c < 3 ) return 3;
+                if ( c < 6 ) return 4;
+                else return 5; 
+            }
+            else { 
+                if (c < 3 ) return 6;
+                if ( c < 6 ) return 7;
+                else return 8; 
+            }
+        } 
     }
 
     public class Board implements PartialSolutionCost, Iterable<Board>{
-        private Cell [][] tabCeldas;
-        private Set<Integer> sets [][];
+        private Set<Integer> setsFilas [];
+        private Set<Integer> setsColumnas[];
+        private Set<Integer> setCuadrantes [];
+        private Cell current;
+        private List<Cell> cellList;
         public Board(String[] input){
-           for (int i = 0, j = 0; i<10 && j<10 ; i++, j++){
-                this.tabCeldas[i][j] = new Cell(input[i].charAt(j), i, j);
-                this.sets [i][j]= new HashSet<Integer>();
+            cellList = new ArrayList<Cell>();
+            for (int i = 0; i<=8; i++){
+                for (int j = 0; j<=8; j++){
+                    char val = (input[i].charAt(j));
+                    Integer valor = (val != '-')? Character.getNumericValue(val) : null;
+                    if (valor != null) { 
+                        this.setsColumnas[i] = new HashSet<Integer>();
+                        this.setsFilas[i] = new HashSet<Integer>();
+                        this.setCuadrantes[i] = new HashSet<Integer>();
+                    }
+                    else { 
+                        cellList.add(new Cell(i,j));
+                    }
+               }
+                
            }
         }
-        public Board board = new Board(input);
+        public Board(Board board){
+            
+        }
         @Override
         public int compareTo(Object o) {
             // TODO Auto-generated method stub
@@ -45,14 +110,14 @@ public class Sudoku9x9 {
 
         @Override
         public boolean isValid() {
-            // TODO Auto-generated method stub
-            return false;
+            return (this.setCuadrantes[current.cuadrante].add(current.valAProbar) && 
+                    this.setsColumnas[this.columna].add(valAProbar) && 
+                    this.setsFilas[this.fila].add(valAProbar));
         }
 
         @Override
         public boolean isFinal() {
-            // TODO Auto-generated method stub
-            return false;
+            return this.cellList.isEmpty();
         }
 
         @Override
@@ -69,15 +134,6 @@ public class Sudoku9x9 {
     }
 
     public void main (String[] args){
-        String [] input = {"-2-5-1-9-",
-                       "8--2-3--4",
-                       "-3--6--7-",
-                       "--1---6--",
-                       "54-----17",
-                       "--2---7--",
-                       "-9--3--8-",
-                       "2--8-4--5",
-                       "-1-9-7-6-"};
         
     }
 
