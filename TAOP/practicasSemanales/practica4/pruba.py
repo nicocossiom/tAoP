8287f41783deff81424642c0413add51343d86ea
NicoCossio <nicocossiom@gmail.com>
2021-12-10 00:29:44 +0100

    en prueba.py -> hecho linked list de costes optimos

--- TAOP/practicasSemanales/practica4/pruba.py
+++ TAOP/practicasSemanales/practica4/pruba.py
@@ -1,8 +1,26 @@
 from typing import List
+from collections import defaultdict
 
-b = [98, 9, 33, 16, 64, 98, 58, 61, 84, 49]
-a = [10, 100, 101, 100]
-c= [10, 300, 10, 400, 500, 20]
+def navLinkedMaxSubSum(i, lista, result):
+        # print(i, lista, result)
+        if i == 0 :
+            result[0] = "F"
+            result[1] = "H"
+            return result
+        elif i==1:
+            result[0] = "H"
+            result[1] = "F"
+            return result
+        prev, origin = lista[i]
+        # print(prev, origin)
+        if origin == i or i==len(lista):
+            # print(f"El {origin} es F")
+            result[origin] = "F"
+        # print(f"Me voy a {prev}")
+        return navLinkedMaxSubSum(prev, lista, result)
+
+
+
 
 def maxSubsequentSum(lista: List[int]):
     size = len(lista)
@@ -10,34 +28,32 @@ def maxSubsequentSum(lista: List[int]):
     if size == 1: return lista[0]
     maxsum: List[int] = [None] * size
     maxsum[0] = lista[0]
-    result2 = []
+    linkedMaxSubSum = [None] * size
     if lista[0] > lista[1]:
         maxsum[1] = lista[0]
-        result2.append("F")
-        result2.append("H")
+
     else:
         maxsum[1] = lista[1]
-        result2.append("H")
-        result2.append("F")
-    result1 = ["F"]
-    result3 = []
+
     for index in range(2, size):
         a = maxsum[index - 1]
         b = maxsum[index - 2] + lista[index]
         if a > b:
-            result3 = result1.copy()
             maxsum[index] = a
-            result3.append("H")
-            result3.append("F")
+            linkedMaxSubSum[index] = index-1, -1
         else:
-            result3 = result2.copy()
             maxsum[index] = b
-            result3.append("H")
-        result1 = result2.copy()
-        result2 = result3.copy()
+            linkedMaxSubSum[index] = index-2, index
+    peorSuma= sum(lista)*2
+    result = navLinkedMaxSubSum(size-1, linkedMaxSubSum, ["H"]*size)
+    print(peorSuma-maxsum[size - 1], linkedMaxSubSum, result)
 
-    peorSuma = 0
-    for elem in lista: peorSuma += elem*2
-    return peorSuma-maxsum[size-1] , maxsum, result2
 
+b = [98, 9, 33, 16, 64, 98, 58, 61, 84, 49]
+a = [10, 100, 101, 100]
+c= [10, 300, 10, 400, 500, 20]
+d = [10,20,300,10,40,50,60]
+print(maxSubsequentSum(a))
+print(maxSubsequentSum(b))
 print(maxSubsequentSum(c))
+print(maxSubsequentSum(d))
